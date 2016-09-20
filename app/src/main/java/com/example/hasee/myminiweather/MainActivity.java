dca7a70f21570b417f2361cf91105233f5421caf
srh2006 <704760782@qq.com>
2016-09-20 22:20:13 +0800

    first commit

---
+++ app/src/main/java/com/example/hasee/myminiweather/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.hasee.myminiweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by hasee on 2016/9/20.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
